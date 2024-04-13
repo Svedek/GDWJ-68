7d4d0fd111f543b33572c239acda4a27ab88a97b
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2024-04-13 12:52:25 -0500

    basic movement added

--- project/scripts/Player.gd
+++ project/scripts/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
